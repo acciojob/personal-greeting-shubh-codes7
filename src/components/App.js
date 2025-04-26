b9d3fa3ad207f3e124936e5039f7e5397632b98a
shubh-codes7 <shubhamcodes7@gmail.com>
2025-04-26 18:27:24 +0530

    f

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,18 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [greet, setGreet] = React.useState('')
+
+  function handleGreet(e){
+    console.log(e.target.value)
+    setGreet(e.target.value)
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <label>Enter your name:</label>
+        <input type="text" onChange={(e)=>handleGreet(e)}/>
+        {greet.trim() !== '' && <p>Hello, {greet}!</p>}
     </div>
   )
 }
